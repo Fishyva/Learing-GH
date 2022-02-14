933137d669307e7695ac0f12265306b6d6ed900c
Shechem <yungrichhoes@gmail.com>
2022-02-14 16:00:55 -0500

    Initial Commit

---
+++ GitLearn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitLearn
+//
+//  Created by Shechem Harrison on 2/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitLearn/GitLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearnApp.swift
+//  GitLearn
+//
+//  Created by Shechem Harrison on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GitLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
